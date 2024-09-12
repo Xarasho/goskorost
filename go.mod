4f9ac615fd09a6f61703b060fbe47bc9e58b5941
Xarasho <alex.olmedo.rojas@gmail.com>
2024-09-12 00:40:51 -0600

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Xarasho/goskorost
+
+go 1.22.0
---
+++ goskorost.go
@@ -0,0 +1,9 @@
+package goskorost
+
+func TestFunc(a, b int) int {
+  return a + b
+}
+
+func TestFunc2(a, b int) int {
+  return a - b
+}
